80c31cf35b05ba8fdd6c8f9ff07137a886ddf66b
jonathanmunamire <jonathanmunamire@gmail.com>
2023-02-22 15:47:58 +0200

    Add Author, Genre and Label Methods

--- UI/main.rb
+++ UI/main.rb
@@ -77,17 +77,27 @@ class Main
   end
 
   def add_book
-    print 'title:'
-    title = gets.chomp
-    print 'Author first name:'
-    first_name = gets.chomp
-    print 'Author last name:'
-    last_name = gets.chomp
+    title = user_input('Enter Book Title: ')
+    color = user_input('Enter Book Color: ')
+    label = Label.new(title, color)
+    genre_name = user_input('Enter Book Genre: ')
+    genre = Genre.new(genre_name)
+    author_firstname = user_input('Enter Author First Name: ')
+    author_lastname = user_input('Enter Author Last Name: ')
+    author = Author.new(author_firstname, author_lastname)
     publish_date = user_input('published_date [DD/MM/YYYY]: ')
     publisher = user_input('publisher: ')
     cover_state = user_input('cover_state [good/bad]: ')
-    @books << Book.new(title, publish_date, publisher, cover_state)
-    @authors << Author.new(first_name, last_name)
+    newbook = Book.new(publish_date, publisher, cover_state)
+    newbook.label = label
+    newbook.genre = genre
+    newbook.author = author
+
+    @books << newbook
+    @genres << genre
+    @labels << label
+    @authors << author
+
     puts 'Book Added Succesfully'
   end
 
@@ -98,15 +108,26 @@ class Main
   end
 
   def add_music_album
-    print 'Enter title:'
-    title = gets.chomp
-    print 'Enter color:'
-    color = gets.chomp
-    print 'publish_date [DD/MM/YYYY]:'
-    published_date = gets.chomp.to_s
-    @labels << Label.new(title, color)
+    title = user_input('Enter Album Title: ')
+    color = user_input('Enter Album Color: ')
+    label = Label.new(title, color)
+    genre_name = user_input('Enter Album Genre: ')
+    genre = Genre.new(genre_name)
+    author_firstname = user_input('Enter Author First Name: ')
+    author_lastname = user_input('Enter Author Last Name: ')
+    author = Author.new(author_firstname, author_lastname)
+    publish_date = user_input('published_date [DD/MM/YYYY]: ')
     spotify = on_spotify
-    @music_albums << Music.new(title, published_date, spotify)
+    newmusic = Music.new(publish_date, spotify)
+    newmusic.label = label
+    newmusic.genre = genre
+    newmusic.author = author
+
+    @music_albums << newmusic
+    @genres << genre
+    @labels << label
+    @authors << author
+
     puts 'Music album and label created'
   end
 
@@ -128,10 +149,27 @@ class Main
   end
 
   def add_game
-    publish_date = user_input('published_date [DD/MM/YYYY]: ')
-    multiplayer = user_input('multiplayer: ')
-    lastplayed = user_input('last played at [DD/MM/YYYY]: ')
-    @games << Game.new(publish_date, multiplayer, lastplayed)
+    title = user_input('Enter Game Title: ')
+    color = user_input('Enter Game Color: ')
+    label = Label.new(title, color)
+    genre_name = user_input('Enter Game Genre: ')
+    genre = Genre.new(genre_name)
+    author_firstname = user_input('Enter Author First Name: ')
+    author_lastname = user_input('Enter Author Last Name: ')
+    author = Author.new(author_firstname, author_lastname)
+    publish_date = user_input('Published_date [DD/MM/YYYY]: ')
+    multiplayer = user_input('Multiplayer: ')
+    lastplayed = user_input('Last played at [DD/MM/YYYY]: ')
+    newgame = Game.new(publish_date, multiplayer, lastplayed)
+
+    newgame.label = label
+    newgame.genre = genre
+    newgame.author = author
+
+    @games << newgame
+    @genres << genre
+    @labels << label
+    @authors << author
   end
 
   def list_all_genres
