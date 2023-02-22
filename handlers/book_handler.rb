56487039655ae7f1ba56294ae6fa5427f57fad54
Alabi12 <wolo22001@gmail.com>
2023-02-22 23:42:09 +0000

    fix linter errors

--- handlers/book_handler.rb
+++ handlers/book_handler.rb
@@ -1,29 +1,29 @@
 require 'json'
 
-module Book_Handler
-    def save_books(books)
-        arr = []
-        path = '../storage/book.json'
+module BookHandler
+  def save_books(books)
+    arr = []
+    path = '../storage/book.json'
 
-        return unless File.exist?(path)
+    return unless File.exist?(path)
 
-        books.map do |book|
-          arr << {date: book.published_date, publisher: book.publisher, cover_state: book.cover_state}
-        end
-
-        File.write(path, JSON.generate(arr))
-      end
-
-      def fetch_books 
-        path = '../storage/book.json'
-
-        return [] unless File.exist?(path)
-
-        json_data = File.read(path)
-        parsed_data = JSON.parse(json_data)
-      
-        parsed_data.map do |book|
-          Book.new(book['date'], book['publisher'], book['cover_state'])
-        end
+    books.map do |book|
+      arr << { date: book.published_date, publisher: book.publisher, cover_state: book.cover_state }
     end
+
+    File.write(path, JSON.generate(arr))
+  end
+
+  def fetch_books
+    path = '../storage/book.json'
+
+    return [] unless File.exist?(path)
+
+    json_data = File.read(path)
+    parsed_data = JSON.parse(json_data)
+
+    parsed_data.map do |book|
+      Book.new(book['date'], book['publisher'], book['cover_state'])
+    end
+  end
 end
--- handlers/genre_handler.rb
+++ handlers/genre_handler.rb
@@ -1,29 +1,29 @@
 require 'json'
 
-module Genre_Handler
-    def save_genre(genres)
-        arr = []
-        path = '../storage/genre.json'
+module GenreHandler
+  def save_genre(genres)
+    arr = []
+    path = '../storage/genre.json'
 
-        return unless File.exist?(path)
+    return unless File.exist?(path)
 
-        genres.map do |e|
-          arr << {name: e.name}
-        end
-
-        File.write(path, JSON.generate(arr))
-      end
-
-      def fetch_genre 
-        path = '../storage/genre.json'
-
-        return [] unless File.exist?(path)
-
-        json_data = File.read(path)
-        parsed_data = JSON.parse(json_data)
-      
-        parsed_data.map do |genre|
-          Genre.new(genre['name'])
-        end
+    genres.map do |e|
+      arr << { name: e.name }
     end
+
+    File.write(path, JSON.generate(arr))
+  end
+
+  def fetch_genre
+    path = '../storage/genre.json'
+
+    return [] unless File.exist?(path)
+
+    json_data = File.read(path)
+    parsed_data = JSON.parse(json_data)
+
+    parsed_data.map do |genre|
+      Genre.new(genre['name'])
+    end
+  end
 end
